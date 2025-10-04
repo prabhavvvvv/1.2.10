30482565c7775587e081c072f3f0974f0f99ccc1
Aryaman Singh <148281236+zealotGuy@users.noreply.github.com>
2025-10-03 21:12:56 -0700

    Update YeddanapudiVasanthakumarSingh_MadLibs.java

--- YeddanapudiVasanthakumarSingh_MadLibs.java
+++ YeddanapudiVasanthakumarSingh_MadLibs.java
@@ -4,16 +4,12 @@ public class LastName_MadLibs {
         System.out.print("Enter a " + wordType + ": ");
         return input.nextLine();
     }
-
     public static String generateStory(String noun1, String noun2, String verb1, String adjective1, String adjective2, String place, String pluralNoun) {
-
         return "One " + adjective1 + " day, I went to the " + place + " with my best friend. We saw a " + noun1 + " that could " + verb1 + " faster than anything else! " +
             "It was so " + adjective2 + " that everyone stopped to watch. " + "Later, we bought some " + pluralNoun +  " and carried them home in a " + noun2 + ".";
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         // Collect user input for each word type
         String noun1 = getWord(input, "noun");
         String noun2 = getWord(input, "another noun");
@@ -23,10 +19,10 @@ public class LastName_MadLibs {
         String place = getWord(input, "place");
         String pluralNoun = getWord(input, "plural noun");
 
-        // Build and display the final story
+        // build & show final story
         String madLibStory = generateStory(noun1, noun2, verb1, adjective1, adjective2, place, pluralNoun);
 
-        System.out.println("\nYour Mad Libs Story\n");
+        System.out.println("Your Mad Libs Story: ");
         System.out.println(madLibStory);
 
         input.close();
