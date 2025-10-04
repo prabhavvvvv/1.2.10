582b92df8fd59a114584e044c622e3bb15307af6
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2025-10-03 20:18:24 -0700

    Update Yeddanapudi_MadLibs.java

--- Yeddanapudi_MadLibs.java
+++ Yeddanapudi_MadLibs.java
@@ -1,35 +1,35 @@
 import java.util.Scanner;
 
 public class Yeddanapudi_MadLibs {
+
+    public static String collectInput(String prompt, Scanner in) {
+        System.out.print(prompt);
+        return in.nextLine();
+    }
+
+    public static void generateStory(String adj1, String noun1, String verb1,
+                                     String adj2, String noun2, String verb2,
+                                     String place) {
+        System.out.println("\nHere's your Mad Libs story:");
+        System.out.println("Once upon a time, there was a " + adj1 + " " + noun1 + " who loved to " + verb1 + ".");
+        System.out.println("One day, it met a " + adj2 + " " + noun2 + " that could " + verb2 + ".");
+        System.out.println("They decided to go on an adventure at " + place + " together.");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter an adjective: ");
-        String adj1 = scanner.nextLine();
+        // collect words from the user
+        String adj1  = collectInput("Enter an adjective: ", scanner);
+        String noun1 = collectInput("Enter a noun: ", scanner);
+        String verb1 = collectInput("Enter a verb: ", scanner);
+        String adj2  = collectInput("Enter another adjective: ", scanner);
+        String noun2 = collectInput("Enter another noun: ", scanner);
+        String verb2 = collectInput("Enter another verb: ", scanner);
+        String place = collectInput("Enter a place: ", scanner);
 
-        System.out.print("Enter a noun: ");
-        String noun1 = scanner.nextLine();
+        generateStory(adj1, noun1, verb1, adj2, noun2, verb2, place);
 
-        System.out.print("Enter a verb: ");
-        String verb1 = scanner.nextLine();
-
-        System.out.print("Enter another adjective: ");
-        String adj2 = scanner.nextLine();
-
-        System.out.print("Enter another noun:");
-        String noun2 = scanner.nextLine();
-
-        System.out.print("Enter another verb: ");
-        String verb2 = scanner.nextLine();
-
-        System.out.print("Enter a place: ");
-        String place = scanner.nextLine();
-
-        System.out.println("Once upon a time, there was a " + adj1 + " " + noun1 + " who loved to " + verb1 + ". ");
-        System.out.println("One day, it met a " + adj2 + " " + noun2 + " that could " + verb2 + ". ");
-        System.out.println("They decided to go on an adventure to " + place + " together. ");
-
-        scanner.close();
-    }
-
+        scanner.close(); 
 }
+
