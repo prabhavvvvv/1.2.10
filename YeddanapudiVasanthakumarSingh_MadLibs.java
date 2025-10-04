6182ed5f1fd479b2cbfb42226184e94aa1525ab6
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2025-10-03 20:31:54 -0700

    Update YeddanapudiVasanthakumarSingh_MadLibs.java

--- YeddanapudiVasanthakumarSingh_MadLibs.java
+++ YeddanapudiVasanthakumarSingh_MadLibs.java
@@ -2,34 +2,42 @@ import java.util.Scanner;
 
 public class Yeddanapudi_MadLibs {
 
-    public static String collectInput(String prompt, Scanner in) {
+    // This function collects the user input whether it be an adjective, noun, verb, or place
+    // Input is returned as a string
+
+    public static String collectUserInput(String prompt, Scanner in) {
         System.out.print(prompt);
         return in.nextLine();
     }
 
-    public static void generateStory(String adj1, String noun1, String verb1,
-                                     String adj2, String noun2, String verb2,
-                                     String place) {
+    // The previous function collected a list of the previous inputs
+    // This function generates the madlib story using those inputs
+    // The completed story is then printed as a string
+
+    public static void generateMadLibStory(String adj1, String noun1, String verb1, String adj2, String noun2, String verb2, String place) {
         System.out.println("\nHere's your Mad Libs story:");
         System.out.println("Once upon a time, there was a " + adj1 + " " + noun1 + " who loved to " + verb1 + ".");
         System.out.println("One day, it met a " + adj2 + " " + noun2 + " that could " + verb2 + ".");
         System.out.println("They decided to go on an adventure at " + place + " together.");
     }
 
+    // The main function runs the program
+    // It collects the user inputs and generates the madlib story
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // collect words from the user
-        String adj1  = collectInput("Enter an adjective: ", scanner);
-        String noun1 = collectInput("Enter a noun: ", scanner);
-        String verb1 = collectInput("Enter a verb: ", scanner);
-        String adj2  = collectInput("Enter another adjective: ", scanner);
-        String noun2 = collectInput("Enter another noun: ", scanner);
-        String verb2 = collectInput("Enter another verb: ", scanner);
-        String place = collectInput("Enter a place: ", scanner);
+        String adj1  = collectUserInput("Enter an adjective: ", scanner);
+        String noun1 = collectUserInput("Enter a noun: ", scanner);
+        String verb1 = collectUserInput("Enter a verb (present-tense): ", scanner);
+        String adj2  = collectUserInput("Enter another adjective: ", scanner);
+        String noun2 = collectUserInput("Enter another noun: ", scanner);
+        String verb2 = collectUserInput("Enter another verb (present-tense): ", scanner);
+        String place = collectUserInput("Enter a place: ", scanner);
 
-        generateStory(adj1, noun1, verb1, adj2, noun2, verb2, place);
+        generateMadLibStory(adj1, noun1, verb1, adj2, noun2, verb2, place);
 
-        scanner.close(); 
+    }
 }
 
