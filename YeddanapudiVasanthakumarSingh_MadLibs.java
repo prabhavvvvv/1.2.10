9bb1df49b241a346452b08c254cfee21076e198a
Vijayesh314 <145930677+Vijayesh314@users.noreply.github.com>
2025-10-10 11:18:10 -0700

    Update YeddanapudiVasanthakumarSingh_MadLibs.java

--- YeddanapudiVasanthakumarSingh_MadLibs.java
+++ YeddanapudiVasanthakumarSingh_MadLibs.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class YeddanapudiVasanthakumarSingh_MadLibs {
-
     // This is the method to get user input for a specific word type
     // returns the entered word
     // Scanner object and word type are passed as parameters
@@ -10,17 +9,26 @@ public class YeddanapudiVasanthakumarSingh_MadLibs {
         return input.nextLine();
     }
 
+    // determines if a word starts with a vowel using indexOf
+    public static String getArticle(String word) {
+        String vowels = "aeiouAEIOU";
+        if (vowels.indexOf(word.charAt(0)) != -1) {
+            return "an";
+        }
+        return "a";
+    }
+
     // This method generates the Mad Libs story using the provided words
     // returns the completed story
     // all the words as then passed as parameters
-
     public static String generateStory(String noun1, String noun2, String verb1, String adjective1, String adjective2, String place, String pluralNoun) {
-        return "One " + adjective1 + " day, I went to the " + place + " with my best friend. We saw a " + noun1 + " that could " + verb1 + " faster than anything else! " +
-            "It was so " + adjective2 + " that everyone stopped to watch. " + "Later, we bought some " + pluralNoun +  " and carried them home in a(n) " + noun2 + ".";
+        String article = getArticle(noun1);
+    
+        return "One " + adjective1 + " day, I went to the " + place + " with my best friend. We saw " + article + " " + noun1 + " that could " + verb1 + " faster than anything else! " +
+            "It was so " + adjective2 + " that everyone stopped to watch. " + "Later, we bought some " + article + pluralNoun + " and carried them home in a(n) " + noun2 + ".";
     }
 
     // this is the main method function
-    
     public static void main(String[] args) {
         Scanner madLib = new Scanner(System.in);
         // get user input for each word type
